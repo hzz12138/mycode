eb91b894bc9440b8119714785daf6f4cc331bbf4
hzz12138 <hhs_zph@mail.imu.edu.cn>
2022-11-30 18:04:16 +0800

    2022.11.30 V1

--- nets/DeepLabV3P.py
+++ nets/DeepLabV3P.py
@@ -17,8 +17,8 @@ from nets.ResNet import BottleNeck
 
 
 class ResNet_V1(ResNet):
-    def __init__(self, block, block_num, num_classes=1000):
-        super(ResNet_V1, self).__init__(block, block_num, num_classes)
+    def __init__(self, block, block_num):
+        super(ResNet_V1, self).__init__(block, block_num)
 
     def forward(self, in_lyr):
         out_lyr = self.stage_1(in_lyr)
@@ -100,14 +100,51 @@ class ASPP(nn.Module):
         return self.final_stage(res)
 
 
+class DeepLabV3P(nn.Module):
+    def __init__(self):
+        super(DeepLabV3P, self).__init__()
+        # 骨干网络
+        self.backbone = ResNet_V1(block=BottleNeck, block_num=[3, 4, 6, 3])
+        in_channel = 2048
+        low_level_channel = 256
+        # 空洞空间金字塔池化
+        self.aspp = ASPP(in_channel=2048,out_channel=256, atrous_rates=[6, 12, 18])
+
+        # 低维信息处理
+        self.low_process = nn.Sequential(
+            nn.Conv2d(in_channels=low_level_channel, out_channels=48, kernel_size=1),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )
+
+        # 高维信息处理
+        # self.high_process = nn.Sequential(
+        #     nn.Conv2d(in_channels=256, out_channels=)
+        # )
+
+
+
+
+    def forward(self,in_lyr):
+        out_lyr = self.backbone(in_lyr)
+        out_lyr = self.aspp(out_lyr[1])
+        return out_lyr
 
 
 
 if __name__ == '__main__':
 
-    asppp = ASPP(32,64,[6,12,18])
-    summary(asppp, input_size=(32, 256, 256))
+    # asppp = ASPP(32,64,[6,12,18]).cuda()
+    # summary(asppp, input_size=(32, 256, 256))
 
-    # test = torch.randn(size=(1, 64, 256, 256))
-    # result = asppp(test)
-    # print(result.shape)
+    # resnet50 = ResNet_V1(block=BottleNeck, block_num=[3,4,6,3]).cuda()
+    # test = torch.randn(size=(1, 3, 256, 256)).cuda()
+    # result = resnet50(test)
+    # print(result[0].shape,result[1].shape)
+    #
+    # summary(resnet50, input_size=(3, 256, 256))
+
+    deep = DeepLabV3P().cuda()
+    test = torch.randn(size=(1, 3, 256, 256)).cuda()
+    result = deep(test)
+    print(result[0].shape, result[1].shape)
